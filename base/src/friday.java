0b5f9e05d3241106fadb6f01e442f6401e090fd5
naveenvpai <pai.naveen357@gmail.com>
2016-07-05 07:19:57 -0700

    Solve friday problem

--- base/src/friday.java
+++ base/src/friday.java
@@ -5,7 +5,6 @@ TASK: friday
 */
 
 import java.io.*;
-import java.util.*;
 
 public class friday {
     public static void main(String[] args) {
@@ -13,6 +12,22 @@ public class friday {
             try {
                 BufferedReader f = new BufferedReader(new FileReader("friday.in"));
                 PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("friday.out")));
+                int period = Integer.parseInt(f.readLine());
+                int startYear = 1900;
+                int day = 13;
+                int[] numThirteenthDays = new int[7]; //0 corresponds to Saturday
+                for (int year = startYear; year < startYear+period; year++) {
+                    int[] months = getDaysInMonths(year);
+                    for (int month : months) {
+                        numThirteenthDays[dayOfTheWeek(day)]++;
+                        day += month;
+                    }
+                }
+                for (int i = 0; i < numThirteenthDays.length; i++) {
+                    if (i!=0) out.print(" ");
+                    out.print(numThirteenthDays[i]);
+                }
+                out.print("\n");
                 out.close();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -21,4 +36,19 @@ public class friday {
             e.printStackTrace();
         }
     }
+
+    private static boolean isLeapYear(int year) {
+        return year%4==0 && (year%100!=0 || year%400==0);
+    }
+
+    private static int[] getDaysInMonths(int year) {
+        int[] result = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
+        if (isLeapYear(year)) result[1] = 29;
+        return result;
+    }
+
+    private static int dayOfTheWeek(int day) {
+        return (day+1)%7;
+    }
 }
+
