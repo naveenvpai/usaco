9b2b22450323a2bd41afb401e3fe5bd6c62ec498
naveenvpai <pai.naveen357@gmail.com>
2017-02-10 18:50:46 -0800

    Success on third try

--- base/src/milk.java
+++ base/src/milk.java
@@ -18,7 +18,10 @@ Need to handle 0 for both!
 
 Solution ideas.
 Order prices, start at lowest keep going until quanitity is fulfilled.
- 
+
+Errors.
+Thought input was quanity-price but it was the other way around.
+Didn't account for HashMap replacing repeats: instead check if the existing price already exists then add to the current quantity.
 */
 
 public class milk {
@@ -32,9 +35,15 @@ public class milk {
                 int numPrices = Integer.valueOf(firstLineInput[1]);
                 SortedMap<Integer, Long> priceToQuantity = new TreeMap<>();
                 for (int i = 0; i < numPrices; i++) {
-                  String[] thisLineInput = f.readLine().split(" ");
-                  //Its given as quantity then price, but we want to order by price
-                  priceToQuantity.put(Integer.valueOf(thisLineInput[1]), Long.valueOf(thisLineInput[0]));
+                    String[] thisLineInput = f.readLine().split(" ");
+                    int price = Integer.valueOf(thisLineInput[0]);
+                    long value = Long.valueOf(thisLineInput[1]);
+                    if(!priceToQuantity.containsKey(price)) {
+                        priceToQuantity.put(price, value);
+                    }
+                    else {
+                        priceToQuantity.replace(price, priceToQuantity.get(price)+value);
+                    }
                 }
                 out.println(findLowestPrice(priceToQuantity,desiredQuantity));
                 out.close();
@@ -49,16 +58,16 @@ public class milk {
     }
 
     static long findLowestPrice(SortedMap<Integer, Long> ptq, long q) {
-      long fulfilled = 0;
-      int totalPrice = 0;
-      int farmer = 0;
-      while (fulfilled != q) {       
-        int pPayed = (Integer)ptq.keySet().toArray()[farmer];
-        long qDemanded = Math.min(q-fulfilled, ptq.get(pPayed));
-        fulfilled += qDemanded;
-        totalPrice += pPayed*qDemanded;
-        farmer++;
-      }
-      return totalPrice;
+        long fulfilled = 0;
+        int totalPrice = 0;
+        int farmer = 0;
+        while (fulfilled != q) {
+            int pPayed = (Integer)ptq.keySet().toArray()[farmer];
+            long qDemanded = Math.min(q-fulfilled, ptq.get(pPayed));
+            fulfilled += qDemanded;
+            totalPrice += pPayed*qDemanded;
+            farmer++;
+        }
+        return totalPrice;
     }
 }
