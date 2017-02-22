1968780725c36a671b664d65279bb906898801ad
naveenvpai <pai.naveen357@gmail.com>
2017-02-21 22:31:16 -0800

    Arrive at a solution much simpler than the journey I took to get to it

--- base/src/skidesign.java
+++ base/src/skidesign.java
@@ -22,17 +22,30 @@ tricky because you want to spread out the reductions as much as possible x^2+y^2
 at the same time, you the tail or head may be favorable depending on how close the min/max and second smallest/largest elements are
 (eg. 3 3 3 5 6 7 20)
 
-Recursively find the cost for each possible head/tail reduction along the way (less thinking, less efficiency) 
+Recursively find the cost for each possible head/tail reduction along the way (less thinking, less efficiency)
+
+Repeatedly add to the head (all lowest elements) and subtract from the tail (all highest elements) in some optimal combination.
+can't guarantee to work for every list!
+
+Medianize the list: adjust it so that each element is within optimal range from the median
+doesn't work because median might not give lowest cost for small lists
+
+Idealize the list: find the center of the interval with width 17 that excludes the least number of elements
+and adjust the excluded elements to be within the range (doesn't work bc distance from range counts as well as number changed)
+
+Find best cost: try every such center above finding the resulting cost at each
 
 Errors.
-O(2^n) doesn't finish for n=50
+O(2^n) doesn't finish for n=50 (maybe worse)
 
 */
 
 public class skidesign {
 
     static int n;
-    static int[] origHills;
+    static int idealRange = 17;
+    static int[] hills;
+
     public static void main(String[] args) {
         try {
             try {
@@ -40,13 +53,13 @@ public class skidesign {
                 PrintWriter out     = new PrintWriter(new BufferedWriter(new FileWriter("skidesign.out")));
 
                 n = Integer.valueOf(f.readLine());
-                origHills = new int[n];
+                hills = new int[n];
                 for (int i = 0; i < n; i++) {
-                    origHills[i] = Integer.valueOf(f.readLine());
+                    hills[i] = Integer.valueOf(f.readLine());
                 }
-                Arrays.sort(origHills);
+                Arrays.sort(hills);
 
-                out.println(minCost(Arrays.copyOf(origHills,n)));
+                out.println(findBestCost(hills));
                 out.close();
             }
             catch (Exception e) {
@@ -58,107 +71,23 @@ public class skidesign {
         }
     }
 
-    /*
-    assume hills is sorted and of length n, divide by zero error?
-    */
-    static int minCost(int[] hills) {
-        while (rangeLeft(hills) > 0) {
-            if (numHeads(hills) < numTails(hills) || rangeLeft(hills) == 1) {
-                incrementHead(hills, Math.min(headSpace(hills), rangeLeft(hills)));
-            } else if (numHeads(hills) == numTails(hills)) {
-                int split = rangeLeft(hills) / 2;
-                incrementHead(hills, Math.min(split,headSpace(hills)));
-                decrementTail(hills, Math.min(split, tailSpace(hills)));
-            } else {
-                decrementTail(hills, Math.min(tailSpace(hills), rangeLeft(hills)));
+    static int findBestCost(int[] hills) {
+        int bestCost = Integer.MAX_VALUE;
+        for (int i = hills[0]; i < hills[n-1]; i++) {
+            int currCost = 0;
+            for (int j = 0; j < n; j++) {
+                int diff = 0;
+                if (hills[j] < i) {
+                    diff = i-hills[j];
+                }
+                else if (hills[j] > i+idealRange) {
+                    diff = hills[j]-(i+idealRange);
+                }
+                currCost += diff*diff;
             }
+            bestCost = Math.min(currCost, bestCost);
         }
-        return evalCost(hills, origHills);
+        return bestCost;
     }
 
-    private static int numTails(int[] hills) {
-        int retVal = 1;
-        for (int i = n-2; i >= 0; i--) {
-            if (hills[i] == hills[n-1]) {
-                retVal++;
-            } else {
-                break;
-            }
-        }
-        return retVal;
-    }
-
-    private static int numHeads(int[] hills) {
-        int retVal = 1;
-        for (int i = 1; i < n; i++) {
-            if (hills[i] == hills[0]) retVal++;
-            else {
-                break;
-            }
-        }
-        return retVal;
-    }
-
-    private static void decrementTail(int[] hills, int amt) {
-        int end = numTails(hills);
-        for (int i = 0; i < end; i++) {
-            hills[n-i-1] -= amt;
-        }
-    }
-
-    private static void incrementHead(int[] hills, int amt) {
-        int end = numHeads(hills);
-        for (int i = 0; i < end; i++) {
-            hills[i] += amt;
-        }
-    }
-
-    private static int headSpace(int[] hills) {
-        return hills[numHeads(hills)]-hills[0];
-    }
-
-    private static int tailSpace(int[] hills) {
-        int retVal = hills[n-1] - hills[n-numTails(hills)-1];
-        return retVal;
-    }
-
-    private static int rangeLeft(int[] hills) {
-        return hills[n-1]-hills[0]-17;
-    }
-
-    /*
-    assume both are sorted and of length n
-    */
-    static int evalCost(int[] hills1, int[] hills2) {
-        int retVal = 0;
-        for (int i = 0; i < n; i++) {
-            int diff = hills1[i] - hills2[i];
-            retVal += diff*diff;
-        }
-        return retVal;
-    }
-
-    /* Assume hills is sorted except for first element and length n*/
-    static void cleanUpBottom(int[] hills) {
-        for (int i = 1; i < n && hills[i] < hills[0]; i++) {
-            if (i == n-1 || hills[i+1] != hills[i]) {
-                int swapHold = hills[i];
-                hills[i] = hills[0];
-                hills[0] = swapHold;
-                break;
-            }
-        }
-    }
-
-    /* Assume hills is sorted except for last element and length n*/
-    static void cleanUpTop(int[] hills) {
-        for (int i = n-2; i >= 0 && hills[i] > hills[n-1]; i--) {
-            if (i == 0 || hills[i-1] != hills[i]) {
-                int swapHold = hills[i];
-                hills[i] = hills[n-1];
-                hills[n-1] = swapHold;
-                break;
-            }
-        }
-    }
 }
