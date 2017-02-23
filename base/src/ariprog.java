78cb5d7273a5aba02435bc4d6286d6ce1b6f9737
naveenvpai <pai.naveen357@gmail.com>
2017-02-22 23:11:22 -0800

    0.3 seconds off on the last test case. they did it just to spite me

--- base/src/ariprog.java
+++ base/src/ariprog.java
@@ -5,6 +5,7 @@ TASK: ariprog
 */
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
 /*
@@ -19,11 +20,38 @@ Errors.
 */
 
 public class ariprog {
+
     public static void main(String[] args) {
         try {
             try {
                 BufferedReader f    = new BufferedReader(new FileReader("ariprog.in"));
                 PrintWriter out     = new PrintWriter(new BufferedWriter(new FileWriter("ariprog.out")));
+
+                int n = Integer.valueOf(f.readLine());
+                int m = Integer.valueOf(f.readLine());
+                HashSet<Integer> bisquares = bisquares(m);
+
+                //trim endponts
+                int numPrinted = 0;
+                int lastBisquare = 2*m*m;
+                for (int b = 1; b <= lastBisquare/(n-1); b++) {
+                    Iterator aIter = bisquares.iterator();
+                    while (aIter.hasNext()) {
+                        int a = (int)aIter.next();
+                        if (a > lastBisquare-b*(n-1)) continue;
+                        int k;
+                        for (k = 0; k < n; k++) {
+                            int ak = a + b * k;
+                            if (!bisquares.contains(ak)) break;
+                        }
+                        if (k == n) {
+                            out.println(a+" "+b);
+                            numPrinted++;
+                        }
+                    }
+                }
+
+                if (numPrinted==0) out.println("NONE");
                 out.close();
             }
             catch (Exception e) {
@@ -33,5 +61,15 @@ public class ariprog {
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    static HashSet<Integer> bisquares(int m) {
+        HashSet<Integer> retVal = new HashSet<>();
+        for (int i = 0; i <= m; i++) {
+            for (int j = i; j <=m; j++) {
+                retVal.add(i*i+j*j);
+            }
+        }
+        return retVal;
     }
 }
